4773b91ed276cc2016052ef5b14f68894bd758c9
Gilberto Hernandez <ghdz89@gmail.com>
2015-06-14 18:15:24 -0500

    Creating list of Houston zipcodes

--- scrapezips.py
+++ scrapezips.py
@@ -1,1 +1,11 @@
 import json
+
+houston = []
+houston_extra = [77204, 77225, 77235, 77251, 77269, 77373, 43451]
+
+for i in range(98):
+	houston.append(77002+i)
+
+houston.extend(houston_extra)
+
+print(houston)
