cb7ca221f8171fc78f035e9efda326c00e851015
Gilberto Hernandez <ghdz89@gmail.com>
2015-06-14 18:34:20 -0500

    Testing dictionary zipcode access

--- scrapezips.py
+++ scrapezips.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 houston = []
 houston_extra = [77204, 77225, 77235, 77251, 77269, 77373, 43451]
@@ -9,3 +10,9 @@ for i in range(98):
 houston.extend(houston_extra)
 
 print(houston)
+
+zip_codes = open(os.getcwd() + '/zip_code_test_file.js', 'r')
+
+zips = json.load(zip_codes)
+
+print(zips['features'][0]['properties']['ZCTA5CE10'])
